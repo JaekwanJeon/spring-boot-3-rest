c7062d227c55635d28a34e44676645d38a15e73a
jaekwan.jeon <jaekwan.jeon@gmail.com>
2023-03-06 22:08:33 +0900

    init

---
+++ src/main/java/com/sample/basic/BasicRestServerApplication.java
@@ -0,0 +1,13 @@
+package com.sample.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicRestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/basic/BasicRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicRestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
